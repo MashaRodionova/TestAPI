4be17d400ed1896a19caea297ae6c81c56b72bda
Masha Rodionova <rodionovamv@gmai.com>
2022-06-16 20:00:40 +0300

    Initial commit

---
+++ src/main/java/ru/netology/api/Main.java
@@ -0,0 +1,14 @@
+package ru.netology.api;
+
+public class Main {
+    public static void main(String[] args){
+       Post post = new Post();
+       post.name = "Иван";
+       post.passport = "7777 # 777777";
+       post.patronymic = "Иванович";
+       post.phone = "7777777";
+       post.surname = "Иванов";
+       post.subscription = true;
+   }
+
+}
